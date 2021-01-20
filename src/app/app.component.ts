bde74f35863344655fffb0e762b251adf44ed4de
Loic Jaouen <loic.jaouen@unil.ch>
2021-01-20 17:37:35 +0100

    initial map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leaflet-tile-test';
+
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      })
+    ],
+    zoom: 8,
+    center: latLng([ 46.5, 6.5 ])
+  };
+
 }
